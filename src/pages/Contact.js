cb3c4f58e227c391a3184dad4dd3b837980495f4
unknown <robert.aleks.zuk@gmail.com>
2021-01-05 20:08:58 +0100

    add action

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,20 +1,13 @@
 import React from "react";
 import MainTemplate from "../templates/MainTemplate";
-//import Button from "../Components/Button";
-//import { Link } from "gatsby";
-import { graphql } from "gatsby";
-import Img from "gatsby-image";
-import styled from "styled-components";
 
-const Form = styled.form`
-  margin-top: 20%;
-`;
+import styled from "styled-components";
 
 const Contact = () => {
   return (
     <form
       name="contact-form"
-      method="post"
+      action="POST"
       data-netlify="true"
       data-netlify-honeypot="bot-field"
     >
@@ -27,15 +20,3 @@ const Contact = () => {
 };
 
 export default Contact;
-
-export const query = graphql`
-  query {
-    file(relativePath: { eq: "img2.jpg" }) {
-      childImageSharp {
-        fixed(width: 500, height: 500) {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
-  }
-`;
