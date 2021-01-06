e5d14f6ee9ba037e40f7acdc5a4156dd11be1246
unknown <robert.aleks.zuk@gmail.com>
2021-01-06 18:42:30 +0100

    add firmik

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,32 @@
 import React from "react";
 import MainTemplate from "../templates/MainTemplate";
-
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import styled from "styled-components";
 
 const Contact = () => {
   return (
-    <form name="contact" method="post">
+    // <form name="contact" method="post">
+    //   <input type="hidden" name="form-name" value="contact" />
+    //   <p>
+    //     <label>
+    //       Your Name: <input type="text" name="name" />
+    //     </label>
+    //   </p>
+    //   <p>
+    //     <label>
+    //       Your Email: <input type="email" name="email" />
+    //     </label>
+    //   </p>
+    //   <p>
+    //     <label>
+    //       Message: <textarea name="message"></textarea>
+    //     </label>
+    //   </p>
+    //   <p>
+    //     <button type="submit">Send</button>
+    //   </p>
+    // </form>
+    <Formik name="contact" method="post">
       <input type="hidden" name="form-name" value="contact" />
       <p>
         <label>
@@ -25,7 +46,7 @@ const Contact = () => {
       <p>
         <button type="submit">Send</button>
       </p>
-    </form>
+    </Formik>
   );
 };
 
