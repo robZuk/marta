c295a0d5d86b5ab982ef389e813a0e6bd2760fb2
unknown <robert.aleks.zuk@gmail.com>
2021-05-09 18:50:19 +0200

    modify package.json

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.div`
+const Container = styled.footer`
   position: fixed;
   width: 100vw;
   height: 20px;
